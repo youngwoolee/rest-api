811a22d1289b48f75e61aa6ad63daaf23cb362f4
Yongwoo, Lee <leeyw910205@gmail.com>
2019-10-06 22:34:54 +0900

    body for badRequest

--- src/main/java/com/joey/restapi/events/EventValidator.java
+++ src/main/java/com/joey/restapi/events/EventValidator.java
@@ -10,8 +10,8 @@ public class EventValidator {
 
     public void validate(EventDto eventDto, Errors errors) {
         if(eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
-            errors.rejectValue("basePrice", "wrongValue", "BasePrice is wrong");
-            errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is wrong");
+            //global error
+            errors.reject("wrongPrices", "Values for prices are wrong");
         }
 
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
--- src/test/java/com/joey/restapi/events/EventControllerTests.java
+++ src/test/java/com/joey/restapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package com.joey.restapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joey.restapi.common.TestDesciption;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +32,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDesciption("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -60,12 +62,8 @@ public class EventControllerTests {
         ;
     }
 
-
-    /**
-     * unknown 파라미터들 테스트
-     * @throws Exception
-     */
     @Test
+    @TestDesciption("입력받을수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -95,6 +93,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDesciption("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -106,6 +105,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDesciption("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
@@ -123,7 +123,15 @@ public class EventControllerTests {
         this.mockMvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(this.objectMapper.writeValueAsString(eventDto)))
-                .andExpect(status().isBadRequest());
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].objectName").exists())
+//                .andExpect(jsonPath("$[0].field").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists())
+//                .andExpect(jsonPath("$[0].rejectedValue").exists())
+
+        ;
 
     }
 
